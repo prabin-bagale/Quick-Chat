7c36737588699119475cafd210198b8a9fec216f
prabin bagale <prabinbagale08@gmail.com>
2025-12-25 11:04:35 +0545

    feat(auth):Created AuthMiddleware for verify

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  // Middleware logic here
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.userId = decodedToken.userId;
+     next();
+  } catch (error) {
+    res.send({
+        message: error.message,
+        success: false
+    });
+  }
+ 
+}
